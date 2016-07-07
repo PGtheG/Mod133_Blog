45896b4ff7a54ec583bc40292888a583b0c1a248
Patrick <patrick.piguet@gmx.ch>
2016-07-07 15:12:29 +0200

    first commit

---
+++ data/inc/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <!--[if ie]><meta content='IE=8' http-equiv='X-UA-Compatible'/><![endif]-->
+    <title>Blog</title>
+    <meta name="viewport" content="width=device-width">
+    <!-- Das favicon.ico und das apple-touch-icon.png in das root Verzeichnis -->
+    <link rel="stylesheet" href="css/screen.min.css">
+    <!--[if lt IE 9]>
+                <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+            <![endif]-->
+</head>
+
+<body>
+    <div class="wrapper">
+        <header>
+            <h1>Blog</h1>
+        </header>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+include('data/inc/header.php');
+?>
+        <section class="grid">
+            <div>25% Column</div>
+            <div>25% Column</div>
+            <div>25% Column</div>
+            <div>25% Column</div>
+        </section>
+<?php
+include('data/inc/footer.php');
+?>
